f712c7e722bf3d46eaac0f577b43b4d179471871
Alesandro Gonzalez <nelsonalesandrog@gmail.com>
2021-01-22 20:04:17 -0600

    deleted unused methods

--- RedisCon.py
+++ RedisCon.py
@@ -8,13 +8,3 @@ class RedisCon:
       self.pool = redis.ConnectionPool(host='localhost', port=6379, password='',db=dbn,decode_responses=True)
       self.r = redis.Redis(connection_pool=self.pool)
 
-   def getAllKeys(self,pattern):
-      keys = []
-      for key in self.r.scan_iter(pattern):
-         keys.append(key)
-      return { "cursor":0, "keys": keys }
-
-   def getKeysCursor(self,cursor,pattern):
-      keys = []
-      keys=self.r.scan(cursor=cursor,count=10,match=pattern)
-      return { "cursor":keys[0],"keys":keys[1] }
